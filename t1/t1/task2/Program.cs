1dc5fab6ce3e0bc408e1982308a8a1fab6834ab0
polina12q3w457 <dershenpolina@gmail.com>
2024-04-16 12:47:43 +0300

    task2

---
+++ t1/t1/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 100 || number > 999)
+            {
+                Console.WriteLine("Введенное число не является трехзначным.");
+                return;
+            }
+
+            int s1 = number / 100;
+            int s2 = (number / 10) % 10;
+            int s3 = number % 10;
+
+            if (s1 == s2 && s2 == s3)
+            {
+                Console.WriteLine("Все цифры числа одинаковы.");
+            }
+            else
+            {
+                Console.WriteLine("Цифры числа не одинаковы.");
+            }
+        }
+    }
+}
