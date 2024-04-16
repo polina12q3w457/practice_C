aef21e308c9fe018850ca2a3d0ca5f5d9b592662
polina12q3w457 <dershenpolina@gmail.com>
2024-04-16 12:51:51 +0300

    task3

---
+++ t1/t1/task3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите порядковый номер дня недели (1-7): ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+                default:
+                    Console.WriteLine("Неверный номер дня недели. Введите число от 1 до 7.");
+                    break;
+            }
+        }
+    }
+}
