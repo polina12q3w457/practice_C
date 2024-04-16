3b8bc7d714ebfa44d9043f099f1b17db8393ce19
polina12q3w457 <dershenpolina@gmail.com>
2024-04-16 14:01:08 +0300

    task7

---
+++ t1/t1/task7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string number = Console.ReadLine();
+
+            bool isAscending = true;
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                if (number[i] < number[i + 1])
+                {
+                    isAscending = false;
+                    break;
+                }
+            }
+
+            if (isAscending)
+            {
+                Console.WriteLine("Цифры числа упорядочены по возрастанию справа налево.");
+            }
+            else
+            {
+                Console.WriteLine("Цифры числа не упорядочены по возрастанию справа налево.");
+            }
+        }
+    }
+}
