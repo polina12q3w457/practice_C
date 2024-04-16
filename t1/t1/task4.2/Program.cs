d3431652f30aab78d3e816a3f23fc31e5b5c97d8
polina12q3w457 <dershenpolina@gmail.com>
2024-04-16 13:32:23 +0300

    task4

---
+++ t1/t1/task4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите значение B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            int i = B;
+            while (i >= A)
+            {
+                Console.WriteLine(Math.Pow(i, 3));
+                i--;
+            }
+        }
+    }
+}
---
+++ t1/t1/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите значение B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = B; i >= A; i--)
+            {
+                Console.WriteLine(Math.Pow(i, 3));
+            }
+        }
+    }
+}
