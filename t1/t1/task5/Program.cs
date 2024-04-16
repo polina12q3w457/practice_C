43ed6230fd7a915ec5af07adbc61f7dd8c2114d4
polina12q3w457 <dershenpolina@gmail.com>
2024-04-16 13:41:18 +0300

    task5

---
+++ t1/t1/task5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            for (int i = A; i <= B; i++)
+            {
+                Console.WriteLine(i);
+                count++;
+            }
+
+            Console.WriteLine("Количество чисел: " + count);
+        }
+    }
+}
