4fc0f3ccf2a9f64c30a0ff34bc1a1662985f856d
polina12q3w457 <dershenpolina@gmail.com>
2024-04-17 12:59:43 +0300

    task1

---
+++ tema2/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task1
+{
+    public class A
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double CalculateExpression()
+        {
+            return 1 / (1 + (double)(a + b) / 2);
+        }
+
+        public int SquareDifference()
+        {
+            return (a - b) * (a - b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A(5, 3);
+            Console.WriteLine("Вычисление выражения: " + obj.CalculateExpression());
+            Console.WriteLine("Возведение в квадрат разности: " + obj.SquareDifference());
+        }
+    }
+}
