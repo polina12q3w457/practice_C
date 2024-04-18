117f0b6c329eb615b9292371f9456f869b07e766
polina12q3w457 <dershenpolina@gmail.com>
2024-04-18 10:28:35 +0300

    task 4-5

---
+++ tema3/task4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите a1: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите a2: ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b2: ");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+
+            long result = SumFor(a1) * SumFor(a2, b2);
+            Console.WriteLine($"Результат: {result}");
+        }
+
+        static long SumFor(int a)
+        {
+            return (long)a * (a + 1) / 2;
+        }
+
+        static long SumFor(int a, int b)
+        {
+            return SumFor(b) - SumFor(a - 1);
+        }
+    }
+}
---
+++ tema3/task5/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace task5
+{
+    public class Matrix
+    {
+        private double[,] _matrix;
+
+        public Matrix(int rows, int cols)
+        {
+            _matrix = new double[rows, cols];
+        }
+
+        public double this[int i, int j]
+        {
+            get { return _matrix[i, j]; }
+            set { _matrix[i, j] = value; }
+        }
+
+        public bool IsSquare()
+        {
+            return _matrix.GetLength(0) == _matrix.GetLength(1);
+        }
+
+        public bool IsDiagonal()
+        {
+            if (!IsSquare())
+                return false;
+
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(1); j++)
+                {
+                    if (i != j && _matrix[i, j] != 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator ==(Matrix m1, Matrix m2)
+        {
+            if (m1._matrix.GetLength(0) != m2._matrix.GetLength(0) || m1._matrix.GetLength(1) != m2._matrix.GetLength(1))
+                return false;
+
+            for (int i = 0; i < m1._matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < m1._matrix.GetLength(1); j++)
+                {
+                    if (m1._matrix[i, j] != m2._matrix[i, j])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(Matrix m1, Matrix m2)
+        {
+            return !(m1 == m2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix(3, 3);
+            Matrix matrix2 = new Matrix(3, 3);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix1[i, j] = i * 3 + j;
+                    matrix2[i, j] = i * 3 + j;
+                }
+            }
+
+            Console.WriteLine(matrix1.IsDiagonal() ? "Матрица диагональная" : "Матрица не диагональная");
+
+            Console.WriteLine(matrix1 == matrix2 ? "Матрицы равны" : "Матрицы не равны");
+        }
+    }
+}
