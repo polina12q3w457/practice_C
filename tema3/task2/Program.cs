3eb611f38345fc9a1a56f407162f02fcedd1a276
polina12q3w457 <dershenpolina@gmail.com>
2024-04-18 09:12:15 +0300

    task2

---
+++ tema3/task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число K: ");
+            int K = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите цифру D1: ");
+            int D1 = Convert.ToInt32(Console.ReadLine());
+            AddRightDigit(D1, ref K);
+            Console.WriteLine($"Результат после добавления D1: {K}");
+
+            Console.Write("Введите цифру D2: ");
+            int D2 = Convert.ToInt32(Console.ReadLine());
+            AddRightDigit(D2, ref K);
+            Console.WriteLine($"Результат после добавления D2: {K}");
+        }
+
+        static void AddRightDigit(int D, ref int K)
+        {
+            if (D >= 0 && D <= 9)
+            {
+                K = K * 10 + D;
+            }
+            else
+            {
+                Console.WriteLine("Цифра D должна быть в диапазоне от 0 до 9.");
+            }
+        }
+    }
+}
