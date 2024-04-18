e9e62fcfecbeceb8c8a2d713ce5ba2f96e282f53
polina12q3w457 <dershenpolina@gmail.com>
2024-04-18 09:04:09 +0300

    task1

---
+++ tema3/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение h: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n x \t y");
+            for (double x = a; x <= b; x += h)
+            {
+                double y = Function(x, a, b);
+                Console.WriteLine($"{x} \t {y}");
+            }
+        }
+
+        static double Function(double x, double a, double b)
+        {
+            if (2 * x <= -5)
+            {
+                return Math.Pow(Math.Cos(2 + x), 2) / a;
+            }
+            else if (-5 <= 2 * x && 2 * x <= 0)
+            {
+                return Math.Pow(b * x, 2) - a;
+            }
+            else // 0 < 2 * x
+            {
+                return (5 * Math.Pow(x, 2) + a * x) / Math.Pow(x, 3);
+            }
+        }
+    }
+}
