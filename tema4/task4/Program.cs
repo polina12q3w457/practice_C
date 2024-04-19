4cc6e0e3178dceeea484b7d2767afb953f022d98
polina12q3w457 <dershenpolina@gmail.com>
2024-04-19 15:05:11 +0300

    task4

---
+++ tema4/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            byte x = 200; byte y = 200;
+            try
+            {
+                byte result = (byte)(x + y);
+                Console.WriteLine("1: {0}", result);
+                result = checked((byte)(x + y));
+                Console.WriteLine("2:", result);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("возникло переполнение");
+            }
+        }
+    }
+}
