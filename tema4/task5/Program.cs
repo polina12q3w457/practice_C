4bcd3e58b3d29a0a19135aeadb33f0b38b832bca
polina12q3w457 <dershenpolina@gmail.com>
2024-04-19 15:14:41 +0300

    task5

---
+++ tema4/task5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            byte n = 1; byte i;
+            try
+            {
+                checked //блок с проверкой
+                {
+                    for (i = 1; i < 10; i++) n *= i;
+                    Console.WriteLine("1: {0}", n);
+                }
+                unchecked //блок без проверки
+                {
+                    n = 1;
+                    for (i = 1; i < 10; i++) n *= i;
+                    Console.WriteLine("2: {0}", n);
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("возникло переполнение");
+            }
+        }
+    }
+}
