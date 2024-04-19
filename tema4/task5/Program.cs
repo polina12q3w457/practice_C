7277327c576f02ec9a3636978b81ac7aaff8ef6a
polina12q3w457 <dershenpolina@gmail.com>
2024-04-19 15:28:09 +0300

    task6

--- tema4/task5/Program.cs
+++ tema4/task5/Program.cs
@@ -7,12 +7,13 @@
             byte n = 1; byte i;
             try
             {
-                checked //блок с проверкой
+                checked 
                 {
                     for (i = 1; i < 10; i++) n *= i;
                     Console.WriteLine("1: {0}", n);
                 }
                 unchecked //блок без проверки
+                unchecked 
                 {
                     n = 1;
                     for (i = 1; i < 10; i++) n *= i;
---
+++ tema4/task6/Program.cs
@@ -0,0 +1,51 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace task6
+{
+    internal class Program
+    {
+        static void genException()
+        {
+            Console.WriteLine("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("b = ");
+            double b = double.Parse(Console.ReadLine());
+            int f = 1;
+            try
+            {
+                for (double i = a; i <= b; ++i)
+                {
+                    try 
+                    {
+                        f = checked((int)(f * i));
+                        if (Math.Abs(f - 1) < 0.000001)
+                        {
+                            throw new DivideByZeroException();
+                        }
+                        Console.WriteLine("y({0})={1:f6}", i, 100 / (f - 1));
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("y({0})= Деление на 0 ", i);
+                    }
+                }
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("ERROR");
+                throw; 
+            }
+        }
+        static void Main()
+        {
+            try
+            {
+                genException();
+            }
+            catch
+            {
+                Console.WriteLine(" НЕИСПРАВИМАЯ ОШИБКА!!!");
+            }
+        }
+    }
+}
