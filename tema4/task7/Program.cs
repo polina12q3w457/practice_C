78a0d46c3224409fe3425abc95acac2ff3e57131
polina12q3w457 <dershenpolina@gmail.com>
2024-04-19 15:36:02 +0300

    task7

---
+++ tema4/task7/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace task7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+{
+                try
+                {
+
+                Console.WriteLine("Введите два числа");
+                    int a = int.Parse(Console.ReadLine());
+                    int b = int.Parse(Console.ReadLine());
+                    Console.WriteLine(a + "/ " +b + " = " +a / b);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Нужно ввести число! ");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Делить на нуль нельзя!");
+                }
+                catch
+                {
+                    Console.WriteLine("Какая - то ошибка ");
+                }
+                finally
+                {
+                    Console.WriteLine("после try-блока ");
+                }
+            }
+        }
+    }
+}
