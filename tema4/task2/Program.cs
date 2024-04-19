0d9092e31779d9161167b1d3bc7a3120a28e39e3
polina12q3w457 <dershenpolina@gmail.com>
2024-04-19 14:09:10 +0300

    task2

---
+++ tema4/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace task2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Введите вещественное число x: ");
+                string input = Console.ReadLine();
+                double x = double.Parse(input);
+
+                if (x <= 0 || x > 3)
+                {
+                    throw new ArgumentOutOfRangeException("x", "Значение x должно быть в диапазоне (0, 3].");
+                }
+
+                double f;
+                if (x < 1)
+                {
+                    f = x - 4;
+                }
+                else
+                {
+                    if (2 * x - 2 == 0)
+                    {
+                        throw new DivideByZeroException("Знаменатель равен нулю.");
+                    }
+                    f = x / (2 * x - 2);
+                }
+
+                Console.WriteLine($"Значение функции f: {f}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введенное значение не является вещественным числом.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
