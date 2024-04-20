7fb419f70909047899c25a754c3c6b93d43ca6b2
polina12q3w457 <dershenpolina@gmail.com>
2024-04-20 08:18:08 +0300

    task1

---
+++ tema5/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 6, 7, 2, 5, 9, 33, 12 };
+            int sum = 0;
+            foreach (int number in array)
+            {
+                if (number % 3 == 0)
+                {
+                    sum += number;
+                }
+            }
+            Console.WriteLine($"Сумма чисел кратных трём = {sum}" );
+        }
+    }
+}
