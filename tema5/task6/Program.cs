7b44b8312beba5fe371917072a0bca1afe0f5686
polina12q3w457 <dershenpolina@gmail.com>
2024-04-20 08:58:42 +0300

    task6

---
+++ tema5/task6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double result = Factorial(n - 1) / Factorial(n);
+            Console.WriteLine($"f({n}) = {result}");
+        }
+
+        static double Factorial(int number)
+        {
+            if (number <= 1)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+    }
+}
