2c1ca98abefe3f4a3b3f1d36a85533ace02ee211
polina12q3w457 <dershenpolina@gmail.com>
2024-04-20 08:48:48 +0300

    task4

---
+++ tema5/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] array = new int[3, 3]; 
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(1, 50); 
+                }
+            }
+
+            Console.Write("Введите номер столбца: ");
+            int columnNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число для проверки: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[i, columnNumber];
+            }
+
+            bool isDivisible = sum % number == 0;
+
+            Console.WriteLine(isDivisible ? "Сумма элементов столбца кратна заданному числу" : "Сумма элементов столбца не кратна заданному числу");
+
+        }
+    }
+}
