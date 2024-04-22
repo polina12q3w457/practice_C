a35de5475034025487ec653529d2781795987deb
polina12q3w457 <dershenpolina@gmail.com>
2024-04-22 13:00:27 +0300

    task2

---
+++ tema6/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace task2
+{
+    class Class
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку: ");
+            StringBuilder a = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Исходная строка: " + a);
+
+            char x = 'Z';
+            char y = 'z';
+
+            for (int i = 0; i < a.Length; ++i)
+                if (a[i] == x)
+                {
+                    a.Insert(i + 1, y);
+                    ++i;
+                }
+
+            Console.WriteLine("Измененная строка: " + a);
+        }
+    }
+}
