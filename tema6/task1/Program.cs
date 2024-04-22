9c007fb38035e0b9c7703ee69cdc2a3794535c3e
polina12q3w457 <dershenpolina@gmail.com>
2024-04-22 11:52:44 +0300

    task1

---
+++ tema6/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите арифметическое выражение:");
+            string expression = Console.ReadLine();
+            bool right = CheckBrackets(expression);
+            if (right)
+                Console.WriteLine("Скобки в выражении расставлены правильно.");
+            else
+                Console.WriteLine("Скобки в выражении расставлены неправильно.");
+        }
+
+        static bool CheckBrackets(string expression)
+        {
+            StringBuilder brackets = new StringBuilder();
+            foreach (char ch in expression)
+            {
+                if (ch == '(')
+                {
+                    brackets.Append(ch);
+                }
+                else if (ch == ')')
+                {
+                    if (brackets.Length == 0 || brackets[brackets.Length - 1] != '(')
+                        return false;
+                    brackets.Remove(brackets.Length - 1, 1);
+                }
+            }
+            return brackets.Length == 0;
+        }
+    }
+}
