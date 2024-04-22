03bfbaebb948be31b966aa0b7393eec33ee46da1
polina12q3w457 <dershenpolina@gmail.com>
2024-04-22 13:14:29 +0300

    task4

---
+++ tema6/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace task4
+{
+    class Class
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите фамилию, имя и отчество: ");
+            string fullName = Console.ReadLine();
+            
+            int sum = 0;
+            foreach (char c in fullName.ToUpper())
+            {
+                if (char.IsLetter(c))
+                {
+                    sum += c - 'А' + 1;
+                }
+            }
+
+            while (sum > 9)
+            {
+                int tempSum = 0;
+                while (sum > 0)
+                {
+                    tempSum += sum % 10;
+                    sum /= 10;
+                }
+                sum = tempSum;
+            }
+
+            Console.WriteLine("Код личности: " + sum);
+        }
+    }
+}
