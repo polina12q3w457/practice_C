dbe28cc44d3c062dc3aff33ae4aca4c841685e57
polina12q3w457 <dershenpolina@gmail.com>
2024-04-23 09:38:33 +0300

    task4

---
+++ tema7/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+namespace task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string text = "Я посетил магазин электроники и увидел новый ноутбук, который стоил 1200$. Затем я пошел в книжный магазин, где купил книгу за 15€. После этого я пошел на рынок и купил яблоки за 3$. В конце дня я посетил ресторан, где обед обошелся мне в 20€.";
+            Regex regex = new Regex(@"(\d+(?:[.,]\d{1,2})?)\s*(€|\$)");
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
