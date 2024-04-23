1edbced9a7f67622ecf02f7a86eb00e969f7ddb7
polina12q3w457 <dershenpolina@gmail.com>
2024-04-23 09:04:39 +0300

    task2

---
+++ tema7/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+namespace task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Однажды, в тихом озере, жил одинокий омар. Он любил обсервировать облака, отражающиеся на поверхности воды. Он всегда мечтал о большом океане, о котором слышал в старых омарьих легендах.";
+            string pattern = "о";
+
+            string newText = Regex.Replace(text, pattern, "оОк");
+
+            Console.WriteLine(newText);
+        }
+    }
+}
+D
