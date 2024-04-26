3b142e4c574c898f6367033f2e0f0de46861272d
polina12q3w457 <dershenpolina@gmail.com>
2024-04-26 13:15:49 +0300

    task2

---
+++ tema8/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task2
+{
+    enum Position
+    {
+        Intern = 120,
+        JuniorDeveloper = 160,
+        SeniorDeveloper = 180,
+        Manager = 150,
+        Director = 140
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Position position in Enum.GetValues(typeof(Position)))
+            {
+                Console.WriteLine($"Должность: {position}");
+                Console.WriteLine($"Часы в месяц: {(int)position}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
