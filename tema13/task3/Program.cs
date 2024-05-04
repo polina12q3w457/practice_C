6c5707006f570b5408b101a8b006f14d28598f81
polina12q3w457 <dershenpolina@gmail.com>
2024-05-04 09:19:23 +0300

    task3

---
+++ tema13/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace task3
+{
+    public class MyInfo
+    {
+        private string name;
+        public event Action<string> NameChanged;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    NameChanged?.Invoke(name);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyInfo myInfo = new MyInfo();
+            myInfo.NameChanged += (newName) => Console.WriteLine("Имя было изменено");
+            myInfo.Name = "Полина";
+        }
+    }
+}
+
