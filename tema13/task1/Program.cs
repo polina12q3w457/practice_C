404b4d85e450277c21d44c6b0000e53c641a9174
polina12q3w457 <dershenpolina@gmail.com>
2024-05-04 09:04:54 +0300

    task1

---
+++ tema13/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace task1
+{
+    class Program
+    {
+        delegate double MyDelegate(double x);
+
+        static double Function1(double x)
+        {
+            return x * x;
+        }
+
+        static double Function2(double x)
+        {
+            return Math.Sin(x);
+        }
+
+        static double Function3(double x)
+        {
+            if (x == 0)
+                throw new DivideByZeroException();
+            return 1 / x;
+        }
+
+        static void Main()
+        {
+            MyDelegate[] delegates = { Function1, Function2, Function3 };
+
+            foreach (var del in delegates)
+            {
+                try
+                {
+                    Console.WriteLine(del(0.5));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Произошла ошибка: {e.Message}");
+                }
+            }
+        }
+    }
+}
