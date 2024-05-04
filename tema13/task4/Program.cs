37308b61ee2c103c8e0745b59e57f364449c04d3
polina12q3w457 <dershenpolina@gmail.com>
2024-05-04 09:21:57 +0300

    task4

---
+++ tema13/task4/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace task4
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher pub = new Publisher();
+            Subscriber1 sub1 = new Subscriber1();
+            Subscriber2 sub2 = new Subscriber2();
+
+            pub.MyEvent += sub1.OnEvent;
+            pub.MyEvent += sub1.OnAnotherEvent;
+            pub.MyEvent += sub2.OnEvent;
+
+            pub.DoSomething();
+
+            Console.WriteLine("Удаляем один обработчик события...");
+            pub.MyEvent -= sub1.OnAnotherEvent;
+
+            pub.DoSomething();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Publisher
+    {
+        public delegate void MyDelegate();
+        public event MyDelegate MyEvent;
+
+        public void DoSomething()
+        {
+            MyEvent?.Invoke();
+        }
+    }
+
+    class Subscriber1
+    {
+        public void OnEvent()
+        {
+            Console.WriteLine("Subscriber1: Обработчик события OnEvent вызван!");
+        }
+
+        public void OnAnotherEvent()
+        {
+            Console.WriteLine("Subscriber1: Обработчик события OnAnotherEvent вызван!");
+        }
+    }
+
+    class Subscriber2
+    {
+        public void OnEvent()
+        {
+            Console.WriteLine("Subscriber2: Обработчик события OnEvent вызван!");
+        }
+    }
+}
