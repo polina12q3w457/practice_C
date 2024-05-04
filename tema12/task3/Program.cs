1050b96791885086a68fe363a763b9cbb99d7b72
polina12q3w457 <dershenpolina@gmail.com>
2024-05-04 08:36:19 +0300

    task3

---
+++ tema12/task3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace task3
+{
+    class Program
+    {
+        public delegate string StringDelegate(string s);
+
+        static void Main(string[] args)
+        {
+            StringDelegate SD;
+
+            SD = ToUpper;
+            Console.WriteLine(SD("hello"));
+
+            SD = ToLower;
+            Console.WriteLine(SD("HELLO"));
+
+            SD = Reverse;
+            Console.WriteLine(SD("hello"));
+        }
+
+        public static string ToUpper(string s)
+        {
+            return s.ToUpper();
+        }
+
+        public static string ToLower(string s)
+        {
+            return s.ToLower();
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
