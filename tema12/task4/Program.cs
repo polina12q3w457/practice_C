de600cc201813828796692f0ca64ebe03b5a9bcd
polina12q3w457 <dershenpolina@gmail.com>
2024-05-04 08:52:53 +0300

    task4

---
+++ tema12/task4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task4
+{
+    using System;
+
+    class Program
+    {
+        delegate int MyDelegate();
+
+        static void Main()
+        {
+            MyDelegate[] delegates = new MyDelegate[5];
+
+            Random rand = new Random();
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                delegates[i] = delegate () { return rand.Next(1, 100); };
+            }
+
+            Func<MyDelegate[], double> average = delegate (MyDelegate[] dels)
+            {
+                double sum = 0;
+                for (int i = 0; i < dels.Length; i++)
+                {
+                    sum += dels.Length;
+                }
+                return sum / dels.Length;
+            };
+
+            Console.WriteLine("Среднее значение: " + average(delegates));
+        }
+    }
+}
+
+
