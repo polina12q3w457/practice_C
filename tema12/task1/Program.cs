8bbbdc9d93ad3d60de9a90bb474b568427532452
polina12q3w457 <dershenpolina@gmail.com>
2024-05-04 08:29:50 +0300

    task1

---
+++ tema12/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace task1
+{
+    class Program
+    {
+        public delegate double CalcFigure(double r);
+
+        static void Main(string[] args)
+        {
+            CalcFigure CF;
+
+            CF = Get_Length;
+            Console.WriteLine("Длина окружности: " + CF(5));
+
+            CF = Get_Area;
+            Console.WriteLine("Площадь круга: " + CF(5));
+
+            CF = Get_Volume;
+            Console.WriteLine("Объем шара: " + CF(5));
+        }
+
+        public static double Get_Length(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        public static double Get_Area(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        public static double Get_Volume(double r)
+        {
+            return 4.0 / 3.0 * Math.PI * Math.Pow(r, 3);
+        }
+    }
+
+}
