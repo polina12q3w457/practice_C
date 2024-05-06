a22ddcfdac2b3d5abf254adb08be6257e12003d1
polina12q3w457 <dershenpolina@gmail.com>
2024-05-06 11:01:38 +0300

    task2

---
+++ tema14/task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Threading;
+namespace task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread t1 = new Thread(SumNumbers);
+            Thread t2 = new Thread(SumNumbers);
+
+            Stopwatch stopwatch1 = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+
+            stopwatch1.Start();
+            t1.Start();
+            t1.Join();
+            stopwatch1.Stop();
+
+            stopwatch2.Start();
+            t2.Start();
+            t2.Join();
+            stopwatch2.Stop();
+
+            Console.WriteLine($"Время выполнения первого потока: {stopwatch1.ElapsedMilliseconds} мс");
+            Console.WriteLine($"Время выполнения второго потока: {stopwatch2.ElapsedMilliseconds} мс");
+        }
+
+        static void SumNumbers()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Сумма чисел от 1 до 10: {sum}");
+        }
+    }
+}
