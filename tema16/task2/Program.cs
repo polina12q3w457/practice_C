1e6be94c798584103072a640de43974ed4415d5f
polina12q3w457 <dershenpolina@gmail.com>
2024-05-07 12:52:42 +0300

    task2

---
+++ tema16/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("New_folder");
+        }
+    }
+}
