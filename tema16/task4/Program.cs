98df5543227079d0b51b598e3e6c031fe7f06d8c
polina12q3w457 <dershenpolina@gmail.com>
2024-05-07 13:14:05 +0300

    task4

---
+++ tema16/task4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = "file1.txt";
+            string file2 = "file2.txt";
+            string tempFile = "temp.txt";
+
+            File.Copy(file1, tempFile, true);
+            File.Copy(file2, file1, true);
+            File.Copy(tempFile, file2, true);
+            File.Delete(tempFile);
+        }
+    }
+}
+    
