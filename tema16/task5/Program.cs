01105b77ea9a2ea8f83bedb0d04c7501af0c9901
polina12q3w457 <dershenpolina@gmail.com>
2024-05-07 13:25:43 +0300

    task5

---
+++ tema16/task5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalNumbers = { 20, 10, 4, -1, -2, 0, 0, 0, -10, 41, 62 };
+            File.WriteAllLines("original.txt", originalNumbers.Select(n => n.ToString()));
+
+            var numbers = File.ReadAllLines("original.txt").Select(int.Parse).ToList();
+
+            var resultNumbers = new List<int>();
+            int sum = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if ((numbers[i] == 0 && sum != 0) || (numbers[i] * sum < 0)) 
+                {
+                    resultNumbers.Add(sum);
+                    sum = numbers[i]; 
+                }
+                else
+                {
+                    sum += numbers[i];
+                }
+            }
+            resultNumbers.Add(sum); 
+
+            File.WriteAllLines("result.txt", resultNumbers.Select(n => n.ToString()));
+
+            Console.WriteLine("Файл 'result.txt' успешно создан.");
+        }
+    }
+}
