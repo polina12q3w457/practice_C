263cc42b7301426b70fa9a80323530485d8a92e1
polina12q3w457 <dershenpolina@gmail.com>
2024-05-07 12:47:17 +0300

    task1

---
+++ tema16/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+    using System.Linq;
+namespace task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "№1.txt";
+            int[] numbers = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 };
+
+            File.WriteAllLines(path, numbers.Select(n => n.ToString()));
+
+            int[] fileNumbers = File.ReadAllLines(path).Select(int.Parse).ToArray();
+
+            int minNumber = fileNumbers.Min();
+
+            int positiveCount = fileNumbers.Count(n => n > 0);
+
+            Console.WriteLine($"Минимальное число: {minNumber}");
+            Console.WriteLine($"Количество положительных элементов: {positiveCount}");
+        }
+    }
+}
