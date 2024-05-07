2668779d22f9336ed05136a6da1ac2dcf894c189
polina12q3w457 <dershenpolina@gmail.com>
2024-05-07 10:01:01 +0300

    task3

---
+++ tema15/task3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+namespace task3
+{
+    public class MyList<T>
+    {
+        private List<T> elements;
+
+        public MyList()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return elements[index];
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return elements.Count;
+            }
+        }
+    }
+
+    public static class MyListExtensions
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] array = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array[i] = list[i];
+            }
+            return array;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            int[] array = myList.GetArray();
+            Console.WriteLine("Элементы в списке:");
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
