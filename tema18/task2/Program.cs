6d0e1dfe94501b872bb3acaba4c6a25c02acf9b7
polina12q3w457 <dershenpolina@gmail.com>
2024-05-09 00:32:04 +0300

    task2

---
+++ tema18/task2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "file.txt"; 
+            Queue<char> nonDigits = new Queue<char>();
+            Queue<char> digits = new Queue<char>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int c;
+                while ((c = sr.Read()) != -1)
+                {
+                    char character = (char)c;
+                    if (Char.IsDigit(character))
+                    {
+                        digits.Enqueue(character);
+                    }
+                    else
+                    {
+                        nonDigits.Enqueue(character);
+                    }
+                }
+            }
+
+            while (nonDigits.Count > 0)
+            {
+                Console.Write(nonDigits.Dequeue());
+            }
+
+            while (digits.Count > 0)
+            {
+                Console.Write(digits.Dequeue());
+            }
+        }
+    }
+}
