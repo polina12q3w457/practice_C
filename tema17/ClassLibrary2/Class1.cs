b813e7a06266762b5b7ad744eafeef1d0b560992
polina12q3w457 <dershenpolina@gmail.com>
2024-05-08 09:17:05 +0300

    task2

---
+++ tema17/ClassLibrary2/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary2
+{
+    public class Bread
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public Bread(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Хлеб {Name} весит {Weight} грамм.";
+        }
+    }
+}
---
+++ tema17/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using ClassLibrary2;
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bread myBread = new Bread("Багет", 200);
+            System.Console.WriteLine(myBread.ToString());
+        }
+    }
+}
