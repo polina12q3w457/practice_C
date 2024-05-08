3d17831d2aa38c87ca13423512528333657ea945
polina12q3w457 <dershenpolina@gmail.com>
2024-05-09 00:21:06 +0300

    task1

---
+++ tema18/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "abc#d##c";
+            Console.WriteLine("Исходная строка: " + input);
+            Console.WriteLine("Преобразованная строка: " + TransformString(input));
+        }
+
+        static string TransformString(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in input)
+            {
+                if (c == '#')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            var arr = stack.ToArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
