46af5e4c8f3566287944a22b6576f2e895a26369
polina12q3w457 <dershenpolina@gmail.com>
2024-05-11 08:14:29 +0300

    task3

---
+++ tema20/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task1 = Task.Run(() => CalculateNewNumber(046)); 
+            Task task2 = task1.ContinueWith(task => Console.WriteLine($"Полученное число: {task.Result}"));
+            Task.WaitAll(task1, task2);
+        }
+        static int CalculateNewNumber(int number)
+        {
+            int firstDigit = number / 100;
+            int lastTwoDigits = number % 100;
+            return lastTwoDigits * 10 + firstDigit;
+        }
+    }
+}
