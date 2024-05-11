9a0fabb4a04008ec2137431f9612c3744d9340e0
polina12q3w457 <dershenpolina@gmail.com>
2024-05-11 08:23:12 +0300

    task4

---
+++ tema20/task4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 15;
+            Parallel.For(A, B + 1, i =>
+            {
+                double result = CalculateFunction(i);
+                Console.WriteLine($"Arccos({i}) = {result}");
+            });
+        }
+        static double CalculateFunction(int x)
+        {
+            if (x >= -5 && x <= 20)
+            {
+                return Math.Acos(x);
+            }
+            else
+            {
+                throw new ArgumentException("Значение x должно быть в диапазоне [-5, 20] для функции Arccos.");
+            }
+        }
+    }
+}
