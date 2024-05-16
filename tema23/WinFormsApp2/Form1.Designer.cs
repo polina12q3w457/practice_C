270e58ebe5f784e958269a77a7db0481e49cc606
polina12q3w457 <dershenpolina@gmail.com>
2024-05-16 14:39:27 +0300

    task2

---
+++ tema23/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(22, 12);
+            chart1.Name = "chart1";
+            chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(694, 341);
+            chart1.TabIndex = 0;
+            chart1.Text = "oo";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 442);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(342, 442);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(499, 441);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "расчёт";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 419);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Xmax";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(342, 419);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Шаг";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(185, 442);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(185, 419);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Xmin";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 481);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(chart1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
---
+++ tema23/WinFormsApp2/Form1.cs
@@ -0,0 +1,41 @@
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Xmin = double.Parse(textBox2.Text);
+            double Xmax = double.Parse(textBox1.Text);
+            double Step = double.Parse(textBox3.Text);
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+            double[] x = new double[count];
+            double[] y = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = Xmin + Step * i;
+                y[i] = Math.Sqrt(x[i]);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+
+            // Изменяем тип графика на линейный
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+
+            chart1.Series[0].Points.DataBindXY(x, y);
+            chart1.Invalidate();
+        }
+    }
+}
