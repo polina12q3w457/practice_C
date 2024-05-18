e1e7a04b9e257aa69128c92e85de58e405d7dc5a
polina12q3w457 <dershenpolina@gmail.com>
2024-05-18 10:24:39 +0300

    fixed

--- tema23/task2/Form1.cs
+++ tema23/task2/Form1.cs
@@ -10,23 +10,14 @@ namespace task2
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            // Создаем объекты Pen и SolidBrush с различными цветами и стилями
             Pen blackPen = new Pen(Color.Black, 3);
             Pen dashedPen = new Pen(Color.Blue, 2) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash };
             SolidBrush redBrush = new SolidBrush(Color.Red);
-
-            // Рисуем контур стула
-            g.DrawRectangle(blackPen, 50, 100, 100, 100); // основание стула
-            g.DrawRectangle(blackPen, 60, 40, 80, 100); // спинка стула
-
-            // Рисуем ножки стула
-            g.DrawLine(dashedPen, 50, 200, 50, 300); // нижняя левая ножка
-            g.DrawLine(dashedPen, 150, 200, 150, 300); // нижняя правая ножка
-
-            // Закрашиваем основание стула
+            g.DrawRectangle(blackPen, 50, 100, 100, 100); 
+            g.DrawRectangle(blackPen, 60, 40, 80, 100); 
+            g.DrawLine(dashedPen, 50, 200, 50, 300); 
+            g.DrawLine(dashedPen, 150, 200, 150, 300); 
             g.FillRectangle(redBrush, 51, 101, 99, 99);
-
-            // Освобождаем ресурсы
             blackPen.Dispose();
             dashedPen.Dispose();
             redBrush.Dispose();
