5f2ca508df40c682dc56c18a5ae587f6d45300d0
polina12q3w457 <dershenpolina@gmail.com>
2024-05-21 21:38:44 +0300

    tema25

---
+++ tema25/task2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using OxyPlot.Series;
+using OxyPlot.Wpf;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var model = new PlotModel { Title = "График функции y=√x" };
+            var series = new FunctionSeries(Math.Sqrt, 0, 10, 0.1, "√x");
+            model.Series.Add(series);
+            plotView.Model = model;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double step;
+            if (double.TryParse(textBox.Text, out step))
+            {
+                plotView.Model.Series.Clear();
+                var series = new FunctionSeries(Math.Sqrt, 0, 10, step, "√x");
+                plotView.Model.Series.Add(series);
+                plotView.InvalidatePlot(true);
+            }
+            else
+            {
+                textBlock.Text = "Неверный шаг!";
+            }
+        }
+    }
+}
---
+++ tema25/task4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button newButton = new Button
+            {
+                Content = "Нажми",
+                Width = 80,
+                Height = 50
+            };
+
+            newButton.Click += (s, args) =>
+            {
+                MainGrid.Children.Remove(newButton);
+            };
+
+            MainGrid.Children.Add(newButton);
+        }
+    }
+}
